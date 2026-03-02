51b0efd7564f11a869f0548f8cca8163418693a8
Damian-K1 <damiankimbrough62@gmail.com>
2026-03-02 10:41:24 -0600

    Fixed the merging error

--- Week 7/in_class.js
+++ Week 7/in_class.js
@@ -1,5 +1,24 @@
-let button = document.getElementById("myButton");
+let form = document.getElementById("taskForm");
+let input = document.getElementById("taskInput");
+let taskList = document.getElementById("taskList");
+let error = document.getElementById("error");
 
-button.addEventListener("click", function() {
-    console.log("Button was clicked!");
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let taskText = input.ariaValueMax.trim();
+
+    if (taskText === "") {
+        error.textContent = "Please enter a task.";
+        return;
+    }
+
+    error.textContent = "";
+
+    let li = document.createElement("li");
+
+    
 });
+
+
+    
