c7f0c2882c56ecc641b263b3aa36a3c59d9f8d1c
Damian-K1 <damiankimbrough62@gmail.com>
2026-04-15 10:17:57 -0500

    Maybe broken, merge changes

--- Project/Project Website/script.js
+++ Project/Project Website/script.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
+
 const toggleButton = document.querySelector(".nav-toggle");
 const navLinks = document.querySelector(".nav-links");
 
 toggleButton.addEventListener("click", () => {
     navLinks.classList.toggle("active");
-=======
-const toggleButton = document.querySelector(".nav-toggle");
-const navLinks = document.querySelector(".nav-links");
-
-toggleButton.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
->>>>>>> faf8b1406ef51b0eac8f62b0bb274abb3688e350
 });
